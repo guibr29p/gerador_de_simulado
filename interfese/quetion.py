05a580438f5dae60e38cd166500c1aaf1a3a21e5
Guilherme Cardozo <gcsmarquesmarques@gmail.com>
2025-11-05 13:20:06 -0300

    questionUI

--- interfese/quetion.py
+++ interfese/quetion.py
@@ -1,24 +1,58 @@
 import streamlit as st
 import pandas as pd
 import os
+from typing import cast
 from dotenv import load_dotenv
 
 load_dotenv()
 collection_dir = os.getenv("collection_dir")
 
 
+def get_key(question_itens, r):
+    for i in range(1, len(question_itens)+1):
+        print(f"verificao valor: {question_itens[f"resposta{i}"]["texto"]} bool: {r == question_itens[f"resposta{i}"]["texto"]}\n")
+        if r == question_itens[f"resposta{i}"]["texto"]:
+            print(f"key function: {f"resposta{i}"}")
+            return f"resposta{i}"
+
+
 def show(file):
     print(f"{collection_dir}/{file}/q{file}.json")
     dt = pd.read_json(f"{collection_dir}/{file}/q{file}.json", orient="records")
 
-    print(dt.head)
-    for _, question_iten in dt.iterrows():
-        
+    for index, question_iten in dt.iterrows():
+        print(index)
+        if "file" not in st.session_state:
+            st.session_state.file = {
+                file: {
+                    index: ""
+                }
+            }
+        elif file not in st.session_state.file.keys():
+            st.session_state.file[file] = {
+                index: ""
+            }
+        else: 
+            st.session_state.file[file][index] = ""
+
         st.write(question_iten["enunciado"])
-        st.radio("resposta ", [
+        r = st.radio("resposta ", [
             question_iten["resposta1"]["texto"],
-            question_iten["resposta1"]["texto"],
-            question_iten["resposta1"]["texto"],
-            question_iten["resposta1"]["texto"],
-            question_iten["resposta1"]["texto"]
+            question_iten["resposta2"]["texto"],
+            question_iten["resposta3"]["texto"],
+            question_iten["resposta4"]["texto"]
         ])
+    
+        st.session_state.file[file][index] = get_key(question_itens=question_iten, r=r) or ""
+        key = st.session_state.file[file][index] 
+        
+        print(question_iten[key]["correta"])
+        if question_iten[key]["correta"] is True:
+            st.write(f":green[resposta: {r}]")
+        elif question_iten[key]["correta"] is False:
+            st.write(f":red[resposta: {r}]")
+        else:
+            st.write(f"resposta: {r}")
+        
+    if st.button("enviar"):
+        st.rerun()
--- test.py
+++ test.py
@@ -1,10 +1,12 @@
-import json
-import pandas as pd
+import streamlit as st
 
+options = [":rainbow[Comedy]", "***Drama***", "Documentary :movie_camera:"]
+genre = st.radio(
+    "What's your favorite movie genre?",
+    options,
+    index=0,
+)
 
-
-path = "data/collection/test1/test.json"
-
-df = pd.read_json(path_or_buf=path, orient="records")
-
-print(df.get("question"))
+st.write("You selected:", genre)
+selected_index = options.index(genre)
+st.write(f"Index of the selected genre is: {selected_index}")
